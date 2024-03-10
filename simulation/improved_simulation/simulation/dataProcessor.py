787f3b3abd189f8781d5d70323ac3967940c1a92
maxachakri <maximilian.achakri@uzh.ch>
2024-03-10 18:00:06 +0100

    Added rb_states saving and processing

--- simulation/improved_simulation/simulation/dataProcessor.py
+++ simulation/improved_simulation/simulation/dataProcessor.py
@@ -11,10 +11,13 @@ class DataProcessor:
         self.start_time = start_time
         self.index_number = index_number
         self.cf_data_dict = []
+        self.rb_state_data_dict = []
 
     def process(self):
         #Process the data here
         self.process_contact_force_data()
+        #self.process_rb_state_data()
+        self.index_number += 1
         pass
     
     def save_data(self):
@@ -28,12 +31,29 @@ class DataProcessor:
             self.cf_data_dict = { 'time':  [time.time()-self.start_time], 'contact' : self.sim_data.net_cf[self.sim_data.panda_idxs][None,:,:]}
         self.cf_data_dict['time'] = np.append(self.cf_data_dict['time'],[time.time()-self.start_time])
         self.cf_data_dict['contact'] = torch.cat((self.cf_data_dict['contact'] , self.sim_data.net_cf[self.sim_data.panda_idxs][None,:,:]))
-        self.index_number += 1
         
+    def process_rb_state_data(self):
+        #process the rb_state data here
+        rb_state = self.sim_data.rb_states
+        if self.index_number == 0:
+            self.rb_state_data_dict = {'time': [time.time()-self.start_time], 'rb_state_position': rb_state[self.sim_data.panda_idxs][0:3], 'rb_state_rotation': rb_state[self.sim_data.panda_idxs][3:7], 'rb_state_velocity': rb_state[self.sim_data.panda_idxs][7:]}
+        self.rb_state_data_dict['time'] = np.append(self.rb_state_data_dict['time'],[time.time()-self.start_time])
+        self.rb_state_data_dict['rb_state_position'] = torch.cat((self.rb_state_data_dict['rb_state_position'], rb_state[self.sim_data.panda_idxs][0:3]))
+        self.rb_state_data_dict['rb_state_rotation'] = torch.cat((self.rb_state_data_dict['rb_state_rotation'], rb_state[self.sim_data.panda_idxs][3:7]))
+        self.rb_state_data_dict['rb_state_velocity'] = torch.cat((self.rb_state_data_dict['rb_state_velocity'], rb_state[self.sim_data.panda_idxs][7:]))
+        pass
+    
     def save_contact_force_data(self):
         #save the contact force data into a pickle file
         dataPath = os.getcwd()+'/DATA/contact_force_data.pickle'
         pkl.dump(self.cf_data_dict, open(dataPath, 'wb'))
+        pass
+        
+    def save_rb_state_data(self):
+        #save the rb_state data into a pickle file
+        dataPath = os.getcwd()+'/DATA/rb_state_data.pickle'
+        pkl.dump(self.rb_state_data_dict, open(dataPath, 'wb'))
+        pass
     
     def contact_detection(self):
         # Check whether a contact happened so that the right rb_state data can be saved
