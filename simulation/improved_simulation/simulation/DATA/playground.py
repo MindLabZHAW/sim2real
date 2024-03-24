5cd9bb8b334c649ae6237ed1d66d8e35ed21955a
maxachakri <maximilian.achakri@uzh.ch>
2024-03-24 14:25:33 +0100

    Added Velocity transformation from Jacobian.

--- simulation/improved_simulation/simulation/DATA/playground.py
+++ simulation/improved_simulation/simulation/DATA/playground.py
@@ -1,6 +1,7 @@
 import pickle
 import torch
 import io
+import numpy as np
 
 class CustomUnpickler(pickle.Unpickler):
     def find_class(self, module, name):
@@ -27,25 +28,62 @@ class PlayGround:
         for key in keys:
             print(key)
             if key == "joint_velocity1":
-                print("Lenght for joint velocity list:",len(self.data[key]))
+                print("Length for joint velocity list:", len(self.data[key]))
             else:
-                #print("Data for:",key, self.data[key])
-                print("Shape for:",key, self.data[key].shape)
-                print("[0] for ",key, self.data[key][0])
+                print("Shape for:", key, self.data[key].shape)
+
+def extract_velocities(jacobian_tensor):
+    """
+    Extracts angular, linear, and joint velocities from a Jacobian tensor.
+
+    Parameters:
+        jacobian_tensor (torch.Tensor): Jacobian tensor with shape [num_entries, num_envs, num_links, 6, num_dofs].
+
+    Returns:
+        angular_velocity (torch.Tensor): Angular velocity tensor with shape [num_entries, num_envs, 3].
+        linear_velocity (torch.Tensor): Linear velocity tensor with shape [num_entries, num_envs, 3].
+        joint_velocity (torch.Tensor): Joint velocity tensor with shape [num_entries, num_envs, num_links, 6].
+    """
+    num_entries = jacobian_tensor.shape[0]
+    num_envs = jacobian_tensor.shape[1]
+
+    print("Jacobian Shape:", jacobian_tensor.shape)
+
+    # Extract Angular Velocity
+    angular_velocity = jacobian_tensor[:, :, -1, -3:, -1]  # Slicing for the last axis
+    print("Angular Velocity Shape (before reshape):", angular_velocity.shape)
+    print("Angular Velocity Total Elements:", angular_velocity.numel())
+
+    angular_velocity = angular_velocity.view(num_entries, num_envs, 3)  # Reshape to [num_entries, num_envs, 3]
+    print("Angular Velocity Shape (after reshape):", angular_velocity.shape)
+
+
+    # Extract Linear Velocity
+    linear_velocity = jacobian_tensor[:, :, -1, :3, -1]  # Slicing for the last axis
+    print("Linear Velocity Shape (before reshape):", linear_velocity.shape)
+
+    linear_velocity = linear_velocity.view(num_entries, num_envs, 3)  # Reshape to [num_entries, num_envs, 3]
+    print("Linear Velocity Shape (after reshape):", linear_velocity.shape)
+
+
+    # Extract Joint Velocity
+    joint_velocity = jacobian_tensor[:, :, :, :6, -1]  # Slicing for the last axis
+    print("Joint Velocity Shape (before reshape):", joint_velocity.shape)
+
+    joint_velocity = joint_velocity.view(num_entries, num_envs, jacobian_tensor.shape[2], 6)
+    print("Joint Velocity Shape (after reshape):", joint_velocity.shape)
+
+    return angular_velocity, linear_velocity, joint_velocity
+
 
 
 playground_jacobian = PlayGround('./jacobian.pickle')   
 print("jacobian")
-jacobian_data = playground_jacobian.print_data_info()     
-playground_cf = PlayGround('./contact_force_data.pickle') 
-print("cf_data")
-cf_data = playground_cf.print_data_info()
-print("rb_state_data")
-playground_rb = PlayGround('./rb_state_data.pickle')
-rb_data = playground_rb.print_data_info()
-#print("dof_state_data")
-#playground_dof = PlayGround('./dof_state_data.pickle')
-#dof_data = playground_dof.print_data_info()
-#print("root_state_data")
-#playground_root = PlayGround('./root_state_data.pickle')
-#root_data = playground_root.print_data_info()
+jacobian_data = playground_jacobian.print_data_info() 
+angular_velocity, linear_velocity, joint_velocity = extract_velocities(playground_jacobian.data['jacobian'])
+print("angular_velocity")
+print(angular_velocity)
+print("linear_velocity")
+print(linear_velocity)
+print("joint_velocity")
+print(joint_velocity)
